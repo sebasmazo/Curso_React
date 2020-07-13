b8aa1d2a1d7ab552727f90638ccf247628b9cc8b
sebasmazo <sebastianmazovelez@gmail.com>
2020-07-13 15:33:31 -0500

    App_React
    Inicio creación APP react

---
+++ 02-intro-javascript/src/Bases/02-template-string.js
@@ -0,0 +1,12 @@
+//Template strings
+const nombre = 'Sebastián';
+const apellido = 'Mazo Vélez';
+
+const nombreCompleto = nombre + ' ' + apellido; //Concatenar sin template strings
+const nombreCompleto1 = `${nombre} ${apellido} `; //Concatenar con template string, dentro de los corchetes es código JavaScript
+
+function getSaludo(nombre) {
+    return 'Hola, ' + nombre;
+}
+
+console.log(`Este es un texto: ${getSaludo(nombreCompleto1)}`);
